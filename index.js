e9103b3ba930377ef146feba42598658d33bd90d
OmarNajjar-Dev <najjar_omar@outlook.com>
2025-02-13 17:19:34 +0200

    Finish the slider section

--- index.js
+++ index.js
@@ -1,6 +1,33 @@
 // Wait for DOM to load
-document.addEventListener('DOMContentLoaded', () => {
-  // Add your form validation code here
+document.addEventListener("DOMContentLoaded", () => {
+  const slider = document.querySelector(".slider");
+  const slides = document.querySelectorAll(".slide");
+  const prevButton = document.getElementById("prevSlide");
+  const nextButton = document.getElementById("nextSlide");
+  var currentIndex = 0;
 
+  function updateSlider() {
+    const translation = -currentIndex * 100;
+    slider.style.transform = `translateX(${translation}%)`;
+  }
+
+  function startAutoSlide() {
+    slideInterval = setInterval(nextSlide, 4000);
+  }
+
+  function prevSlide() {
+    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    updateSlider();
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % slides.length;
+    updateSlider();
+  }
+
+  startAutoSlide();
+  prevButton.addEventListener("click", prevSlide);
+  nextButton.addEventListener("click", nextSlide);
+  // Add your form validation code here
   // Add your EmailJS integration code here
 });
